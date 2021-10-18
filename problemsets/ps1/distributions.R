400fab9f69735c924fc6e209aa11552d381c9f01
ben-domingue <ben.domingue@gmail.com>
2021-10-18 12:32:45 -0700

    ps1

--- problemsets/ps1/distributions.R
+++ problemsets/ps1/distributions.R
@@ -21,14 +21,15 @@ for (i in 1:length(L)) lines(L[[i]][,-1],lwd=2,col=cols[i])
 
 ##the binomial distribution
 
-##the CLT
+##the CLT, https://en.wikipedia.org/wiki/Central_limit_theorem
+#Which is relevant, N or n, in terms of having the CLT apply?
 fun<-function(N,n,p=0.5) { #N is the number of people, n is the number of tosses per person
     x<-rbinom(N,n,p)
     x<-(x-mean(x))/sd(x)
-    hist(x,main='',freq=FALSE)
+    hist(x,main='',freq=FALSE,yaxt='n')
     xv<-seq(-3,3,length.out=1000)
     lines(xv,dnorm(xv))
-    legend("topleft",bty='n',paste(N,n))
+    legend("topleft",bty='n',c(paste0("N=",N),paste0("n=",n)))
 }
 par(mfrow=c(5,5),mar=c(3,3,1,1),mgp=c(2,1,0),oma=rep(0,4))
 for (N in c(10,50,100,1000,5000)) for (n in c(5,25,100,1000,5000)) fun(N,n)
