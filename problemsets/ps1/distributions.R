af90fdb8c4e60e0791675a2c45984e88a61c09bb
ben-domingue <ben.domingue@gmail.com>
2022-03-17 12:25:30 -0700

    ps1

--- problemsets/ps1/distributions.R
+++ problemsets/ps1/distributions.R
@@ -1,9 +1,9 @@
 ############################################################################################################
-##bernoulli distribution
-##how good of an estimate of p is the mean? interplay between the magnitude of p and the sample size
+##A. bernoulli distribution
+##a. how good of an estimate of p is the mean? interplay between the magnitude of p and the sample size
 
 p.est<-list()
-for (N in c(4,10,100,1000)) {
+for (N in c(5,10,50,100,1000)) {
     for (p in seq(0.5,0.99,length.out=25)) {
         m<-numeric()
         for (i in 1:1000) {
@@ -16,11 +16,13 @@ for (N in c(4,10,100,1000)) {
 
 x<-data.frame(do.call("rbind",p.est))
 L<-split(x,x[,1])
-cols<-c("pink","red","blue","black")
+cols<-c("pink","red","blue","gray","black")
 plot(NULL,ylim=c(0,0.1),xlim=range(x[,2]),xlab='p',ylab='mse')
 for (i in 1:length(L)) lines(L[[i]][,-1],lwd=2,col=cols[i])
+legend("topright",bty='n',fill=cols,names(L))
 
-##the binomial distribution
+############################################################################################################
+##b. the binomial distribution
 ntrial<-15
 x<-rbinom(10000,ntrial,.5)
 y<-numeric()
@@ -29,9 +31,12 @@ for (i in 1:10000) {
     for (j in 1:ntrial) z[j]<-rbinom(1,1,.5)
     y[i]<-sum(z)
 }
+x<-factor(x,levels=0:ntrial)
+y<-factor(y,levels=0:ntrial)
 plot(as.numeric(table(x)),as.numeric(table(y))); abline(0,1)
 
-##the CLT, https://en.wikipedia.org/wiki/Central_limit_theorem
+############################################################################################################
+##c. the CLT, https://en.wikipedia.org/wiki/Central_limit_theorem
 #Which is relevant, N or n, in terms of having the CLT apply?
 fun<-function(N,n,p=0.5) { #N is the number of people, n is the number of tosses per person
     x<-rbinom(N,n,p)
@@ -45,14 +50,14 @@ par(mfrow=c(5,5),mar=c(3,3,1,1),mgp=c(2,1,0),oma=rep(0,4))
 for (N in c(10,50,100,1000,5000)) for (n in c(5,25,100,1000,5000)) fun(N,n)
 
 ############################################################################################################
-##the normal distribution
+##C. the normal distribution
 ##Let's attempt to recover some of the most useful facts about the normal distribution from simulation
 
-##What proportion of the distribution is more than 1.96 from the mean(=0)?
+##a.i. What proportion of the distribution is more than 1.96 from the mean(=0)?
 x<-rnorm(50000,mean=0,sd=1)
 sum(abs(x)>1.96)/length(x) #what is this telling us?
 
-##What does the sum of two normal variables look like?
+##a.iiWhat does the sum of two normal variables look like?
 N<-1000
 m1<-3
 sd1<-2
@@ -79,7 +84,7 @@ plot(sd2,z[,2])
 
 ##Note: Analytic derivations of these facts would be preferable for any number of reasons. The goal here is to get our hands dirty with some real simulations but simulations are not always the right tool for the job! 
 
-##Now let's look at a bivariate normal distribution
+##b. Now let's look at a bivariate normal distribution
 library(MASS)
 x<-mvrnorm(500000,mu=rep(0,2),Sigma=matrix(c(1,.5,.5,1),2,2))
 ##What is the distribution of a conditional look like? So if we look at the probability density when the second variable is 1.5 (or some other value)?
