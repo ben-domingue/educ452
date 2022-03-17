30363f41c61f2753990b93408034405a61da346e
ben-domingue <ben.domingue@gmail.com>
2022-03-17 11:18:52 -0700

    reg

--- regression_example.R
+++ regression_example.R
@@ -10,7 +10,7 @@ summary(m)
 
 ######################################################
 ##What if we start to make changes to the DGM and ask about their relationship to the empirical data?
-##We're going to look at power (probability that we correctly reject the null hypothesis)and FDR (rate of type 1 error, mistaken rejection of actually true null hypothesis) for a few examples
+##We're going to look at power (probability that we correctly reject the null hypothesis)and type 1 error rate (type 1 error: mistaken rejection of actually true null hypothesis) for a few examples
 
 ######################################################
 ##UNOBSERVED CONFOUNDERS
@@ -20,20 +20,21 @@ N<-1000
 b<-0.00 ##important!
 mat<-list()
 for (rho in seq(0,.7,by=.1)) {
-    fdr<-numeric()
+    t1er<-numeric()
     for (i in 1:500) {
         xz<-mvrnorm(N,rep(0,2),Sigma=matrix(c(1,rho,rho,1),2,2))
         x<-xz[,1]
         z<-xz[,2]
         y<-b*x+.1*z+rnorm(N)  ##note that x does not impact y since b=0!
         m<-lm(y~x)
-        fdr[i]<-summary(m)$coef[2,4]<.05
+        t1er[i]<-summary(m)$coef[2,4]<.05
     }
-    mat[[as.character(rho)]]<-c(rho,mean(fdr))
+    mat[[as.character(rho)]]<-c(rho,mean(t1er))
 }
-plot(do.call("rbind",mat),xlab="rho",ylab="false discovery rate",pch=19,cex=2,ylim=c(0,1))
+plot(do.call("rbind",mat),xlab="cor(x,z)",ylab="type 1 error rate",pch=19,cex=2,ylim=c(0,1))
 abline(h=0.05,col='gray')
-##in the above, we see that FDR increases with rho. that makes sense. z contains information about x due to their correlation and does impact y. if we don't observe z, we might mistakenly think x and y are associated.
+##in the above, we see that T1ER increases with rho=cor(x,z).
+##that makes sense. z contains information about x due to their correlation and does impact y. if we don't observe z, we might mistakenly think x and y are associated.
 ##questions to ask: how does the shape of that curve depend upon N? upon the coefficient associated with z in y's assignment? 
 
 ######################################################
