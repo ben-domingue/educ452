387ed49eabb2c04470bec81a323aa2b3c8be3bf2
ben-domingue <ben.domingue@gmail.com>
2022-04-29 12:12:44 -0700

    ps3

--- problemsets/ps3/differentiation.R
+++ problemsets/ps3/differentiation.R
@@ -3,7 +3,10 @@ load("LA_nice_sub.Rdata")
 ##Let's first look at the empirical data
 library(lme4)
 ma<-df[df$subject=="MATHEMATICS",]
-mod<-lmer(scale_score_std~scale_score_std_lag_1+in.title1+ell+join.after.k+factor(grade)+factor(year)+(1|teacher_id),ma)
+m<-by(ma$scale_score_std_lag_1,ma$teacher_id,mean,na.rm=TRUE)
+m<-data.frame(teacher_id=names(m),lm=as.numeric(m))
+ma<-merge(ma,m)
+mod<-lmer(scale_score_std~scale_score_std_lag_1+lm+in.title1+ell+join.after.k+factor(grade)+factor(year)+(1|teacher_id),ma)
 te<-ranef(mod)$teacher_id
 te<-data.frame(teacher_id=rownames(te),te=te[,1])
 s<-by(ma$scale_score_std_lag_1,ma$teacher_id,sd,na.rm=TRUE)
@@ -12,9 +15,14 @@ x<-merge(te,s)
 
 plot(x$sd,x$te,pch=19,cex=.5)
 abline(lm(te~sd,x))
-
+cc<-cor(x$sd,x$te,use='p')
+     
 ##meh, not much going on (and, well, the opposite of my intuition!), but let's turn to a different question: would we be able to detect it if true?
-
+load("LA_nice_sub.Rdata") 
+ma<-df[df$subject=="MATHEMATICS",]
+m<-by(ma$scale_score_std_lag_1,ma$teacher_id,mean,na.rm=TRUE)
+m<-data.frame(teacher_id=names(m),lm=as.numeric(m))
+ma<-merge(ma,m)
 sim<-function(ma,scale) {
     ma$scale_score_std<-NA
     sd.error<-.55
@@ -24,19 +32,21 @@ sim<-function(ma,scale) {
     s<-data.frame(teacher_id=names(s),sd=as.numeric(s))
     s$sd<-(s$sd-mean(s$sd,na.rm=TRUE))/sd(s$sd,na.rm=TRUE)
     s<-s[!is.na(s$sd),]
-    ##
-    te<-rnorm(length(unique(ma$teacher_id)),
+    ##here is where we build in the 'structure' that we are going to then investigaet
+    te<-rnorm(nrow(s),
               mean=scale*s$sd, #note scale
               sd=sd.teacherfx)
-    te<-data.frame(teacher_id=unique(ma$teacher_id),te=te)
+    te<-data.frame(teacher_id=s$teacher_id,te=te)
     ma<-merge(ma,te)
     ma$scale_score_std<-.7*ma$scale_score_std_lag_1+ma$te+rnorm(nrow(ma),mean=0,sd=sd.error)
     ##
     ma<-ma[!is.na(ma$scale_score_std_lag_1),]
-    mod<-lmer(scale_score_std~scale_score_std_lag_1+in.title1+ell+join.after.k+factor(grade)+factor(year)+(1|teacher_id),ma)
+    mod<-lmer(scale_score_std~scale_score_std_lag_1+lm+in.title1+ell+join.after.k+factor(grade)+factor(year)+(1|teacher_id),ma)
     te<-ranef(mod)$teacher_id
     te<-data.frame(teacher_id=rownames(te),te=te[,1])
     x<-merge(te,s)
-    cor(x$te,x$sd)
+    cor(x$te,x$sd) #so i'm returning correlations of estimated teacher effects and class SD of prior abilities
 }
-for (scale in seq(-1,1,by=.5)) print(sim(ma=ma,scale=scale))
+for (scale in seq(-.25,.25,length.out=5)) print(c(scale,sim(ma=ma,scale=scale)))
+print("reminder of empirical results")
+print(cc)
