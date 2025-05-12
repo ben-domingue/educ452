690b9c5c7910a8ddf8dcf40dec3ae8d4a6764431
Ben Domingue <ben.domingue@gmail.com>
2025-05-12 07:37:01 -0700

    dynarski

--- aid/model.R
+++ aid/model.R
@@ -1,11 +1,12 @@
 x<-read.csv("Dynarski2003.csv")
 
+x$fatherdec<-ifelse(x$fatherdec=="Father deceased",1,0)
 
 x$before<-ifelse(x$yearsr<=81,1,0)
 L<-split(x,paste(x$before,x$fatherdec))
 f<-function(x) weighted.mean(x$coll,x$wt88)
-lapply(L,f) #compare to table 2
+lapply(L,f) #compare to table 1
 
-##maybe look at ols error
+
 m<-lm(coll~fatherdec*before,x,weights=x$wt88)
-summary(m) ##interaction estimate of 0.256 in table 3; i get 0.18
+summary(m) ##compare to table 2
