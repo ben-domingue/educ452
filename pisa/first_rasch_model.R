e6eb760629fd21276ea0a32c549969ef490c3962
Ben Domingue <ben.domingue@gmail.com>
2025-05-21 05:39:25 -0700

    pisa

--- pisa/first_rasch_model.R
+++ pisa/first_rasch_model.R
@@ -3,8 +3,17 @@
 ## the main goal is to get oriented with the key output 
 ## that we get from applications of these models to item response data
 
-resp1 <- read.table("https://github.com/ben-domingue/252L/raw/master/data/emp-rasch.txt", header=FALSE)
-resp1 #can you make sense of this data structure? hopefully fairly straightforward.
+## simulating data from the Rasch model
+sim<-function(th,b) { # th are abilities, b are difficulties
+    np<-length(th)
+    ni<-length(b)
+    k<-outer(th,b,'-')
+    p<-1/(1+exp(-k))
+    resp<-p
+    for (i in 1:ncol(resp)) resp[,i]<-rbinom(np,1,p[,i])
+    data.frame(resp)
+}
+resp1<-sim(th=rnorm(1000),b=rnorm(25))
 
 ## fit the rasch model
 library(mirt) # might have to run install.packages("mirt")
@@ -15,16 +24,7 @@ m1 #here is the object containing the estimated rasch model. it contains lots of
 plot(m1, type = "trace") ## which is the easiest item? the most difficult item?
 
 ## we can use the below to get item parameters
-coef(m1)
-
-##that was unwieldy, here is a better way of getting that output
-get_coef <- function(mod) {
-  co <- coef(mod)
-  co <- co[-length(co)]#why do i get rid of this last bit?
-  do.call("rbind", co)
-}
-coef<-get_coef(m1) #first column is alpha (note that they don't vary), second column is 'difficulty', ignore third and fourth [these relate to asymptotes]
-coef
+coef(m1,simplify=TRUE,IRTpars=TRUE)
 
 ## in particular, i would look over this closely vis-a-vis the relevant part of the mirt manual:
 ## Rasch Only one intercept estimated, and the latent variance of
@@ -33,16 +33,13 @@ coef
 ##      'gpcm' below).
 ##           P(x = 1|theta, d) = \frac{1}{1 + exp(-(theta + d))}      
 
-##note that there is something different when we compare "our" version of the Rasch model to the mirt version.
-##It's very important that you note this difference!
-##so, be able to make sure you can explain this!
-plot(colMeans(resp1,na.rm=TRUE),coef[,2],xlab="p-values",ylab="raw mirt estimates")
+##note the theta+d bit: we are getting -d reported to us given the IRTpars=TRUE argument
 
 ## here is a fun way of looking at comparing the estimated icc to empirical data
 itemfit(m1, empirical.plot = 3)
 
 ##as a comparison
 resp2<-resp1
-resp2$V55<-rbinom(nrow(resp2),1,.5)
+resp2[,1]<-rbinom(nrow(resp2),1,.5)
 m2 <- mirt(resp2, 1, itemtype = "Rasch")
-itemfit(m2, empirical.plot =55)
+itemfit(m2, empirical.plot =1)
