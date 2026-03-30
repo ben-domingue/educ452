03c51e6f26b02c44cf66dd62db7163c70be4e4cb
ben-domingue <ben.domingue@gmail.com>
2026-03-30 06:20:38 -0700

    ps1

--- ps1/distributions_bernoulli.R
+++ ps1/distributions_bernoulli.R
@@ -1,42 +1,6 @@
-############################################################################################################
-##A. bernoulli distribution
-##a. how good of an estimate of p is the mean? interplay between the magnitude of p and the sample size
-
-p.est<-list()
-for (N in c(5,10,50,100,1000)) {
-    for (p in seq(0.5,0.99,length.out=25)) {
-        m<-numeric()
-        for (i in 1:1000) {
-            coins<-rbinom(N,1,p)
-            m[i]<-mean(coins)
-        }
-        p.est[[paste(N,p)]]<-c(N,p,mean((m-p)^2))
-    }
-}
-
-x<-data.frame(do.call("rbind",p.est))
-L<-split(x,x[,1])
-cols<-c("pink","red","blue","gray","black")
-plot(NULL,ylim=c(0,0.1),xlim=range(x[,2]),xlab='p',ylab='mse')
-for (i in 1:length(L)) lines(L[[i]][,-1],lwd=2,col=cols[i])
-legend("topright",bty='n',fill=cols,names(L))
 
 ############################################################################################################
-##b. the binomial distribution
-ntrial<-15
-x<-rbinom(10000,ntrial,.5)
-y<-numeric()
-for (i in 1:10000) {
-    z<-numeric()
-    for (j in 1:ntrial) z[j]<-rbinom(1,1,.5)
-    y[i]<-sum(z)
-}
-x<-factor(x,levels=0:ntrial)
-y<-factor(y,levels=0:ntrial)
-plot(as.numeric(table(x)),as.numeric(table(y))); abline(0,1)
-
-############################################################################################################
-##c. the CLT, https://en.wikipedia.org/wiki/Central_limit_theorem
+##a. the CLT, https://en.wikipedia.org/wiki/Central_limit_theorem
 #Which is relevant, N or n, in terms of having the CLT apply?
 fun<-function(N,n,p=0.5) { #N is the number of people, n is the number of tosses per person
     x<-rbinom(N,n,p)
